c85da0ee8d3f1a45852c11fef1a9b067e74950b2
Billy <billy.bouchard03@gmail.com>
2018-03-13 16:50:54 -0400

    Add second pane completed

--- pages/script/main.js
+++ pages/script/main.js
@@ -1,3 +1,25 @@
-function getPages(pageName) {
+$(document)
+  .ready(function () {
+    $('.pane-switcher')
+      .bind('click', function () {
+        switchActive(this)
+      })
+    $("#submit-file")
+      .bind('click', function () {
+        switchActive(this)
+      })
+  })
 
+/*********************************************************************
+ * Function that use the active element href to get the pane id
+ * to which it need to switch
+ *********************************************************************/
+switchActive = function (object) {
+  $('.active-pane')
+    .addClass('reserve-pane')
+    .removeClass('active-pane')
+  $($(object)
+      .attr('href'))
+    .addClass('active-pane')
+    .removeClass('reserve-pane')
 }
